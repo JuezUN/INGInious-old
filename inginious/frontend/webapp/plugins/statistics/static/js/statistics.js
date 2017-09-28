85254ee41e17dc0d380e925e5da2c335869a3550
larranaga <nlarranagac@unal.edu.co>
2017-09-28 01:26:25 -0500

    cleaned code that displays charts

--- inginious/frontend/webapp/plugins/statistics/static/js/statistics.js
+++ inginious/frontend/webapp/plugins/statistics/static/js/statistics.js
@@ -1,56 +1,32 @@
 var toggle_normalize_submissions_per_tasks = false;
 var toggle_normalize_best_submissions_per_tasks = false;
 
-function transformObjectToPlotData(data, verdict, color_category) {
-
-  var plotData = {
-    x: [],
-    y: [],
-    name: verdict,
-    marker: {color: color_category},
-    type: 'bar'
-  };
-
-  for(var i = 0; i < data.length; ++i) {
-
-    if(data[i].summary_result === verdict){
-      plotData.x.push(data[i].task_id);
-      plotData.y.push(data[i].count);
-    }
-
-  }
-  return plotData;
-}
-
 function plotVerdictStatistics(id_div, statistics){
   var data = statistics.by_verdict;
 
   var title = "Submissions Vs Verdicts (ALL)";
 
-  if(toggle_normalize_submissions_per_tasks){
-    plotVerdictStatisticsNormalized(id_div, data, title);
-    toggle_normalize_submissions_per_tasks = !toggle_normalize_submissions_per_tasks
-  }else{
-    plotVerdictStatisticsRaw(id_div, data, title);
-    toggle_normalize_submissions_per_tasks = !toggle_normalize_submissions_per_tasks
-  }
+  plotVerdictStatisticsChart(id_div, data, title, toggle_normalize_submissions_per_tasks);
+  toggle_normalize_submissions_per_tasks = !toggle_normalize_submissions_per_tasks
 }
 
 function plotBestVerdictStatistics(id_div, statistics){
   var data = statistics.best_by_verdict;
 
   var title = "Submissions Vs Verdicts (Best)";
-
-  if(toggle_normalize_best_submissions_per_tasks){
-    plotVerdictStatisticsNormalized(id_div, data, title);
-    toggle_normalize_best_submissions_per_tasks = !toggle_normalize_best_submissions_per_tasks
-  }else{
-    plotVerdictStatisticsRaw(id_div, data, title);
-    toggle_normalize_best_submissions_per_tasks = !toggle_normalize_best_submissions_per_tasks
-  }
+  plotVerdictStatisticsChart(id_div, data, title, toggle_normalize_best_submissions_per_tasks);
+  toggle_normalize_best_submissions_per_tasks = !toggle_normalize_best_submissions_per_tasks
 }
 
-function createObjectToPlotDataNormalized(data, data_count_obj, verdict, color_category) {
+function getDataNormalized(data_entry, data_count_obj){
+    return data_entry.count/data_count_obj[data_entry.task_id]*100;
+}
+
+function getData(data_entry, data_count_obj){
+    return data_entry.count;
+}
+
+function createObjectToPlotData(data, data_count_obj, verdict, color_category, get_function) {
 
   var plotData = {
     x: [],
@@ -64,15 +40,13 @@ function createObjectToPlotDataNormalized(data, data_count_obj, verdict, color_c
 
     if(data[i].summary_result === verdict){
         plotData.x.push(data[i].task_id);
-        plotData.y.push((data[i].count/data_count_obj[data[i].task_id])*100);
+        plotData.y.push(get_function(data[i], data_count_obj));
     }
-
   }
-
   return plotData;
 }
 
-function plotVerdictStatisticsNormalized(id_div, data, statistic_title) {
+function plotVerdictStatisticsChart(id_div, data, statistic_title, normalized) {
 
   var data_count_obj = {};
 
@@ -84,13 +58,22 @@ function plotVerdictStatisticsNormalized(id_div, data, statistic_title) {
     }
   }
 
-  var compilation_error_data = createObjectToPlotDataNormalized(data, data_count_obj, "COMPILATION_ERROR", 'rgb(236,199,6)');
-  var time_limit_data = createObjectToPlotDataNormalized(data, data_count_obj,"TIME_LIMIT_EXCEEDED", 'rgb(50,120,202)');
-  var memory_limit_data = createObjectToPlotDataNormalized(data, data_count_obj,"MEMORY_LIMIT_EXCEEDED", 'rgb(119,92,133)');
-  var runtime_error_data = createObjectToPlotDataNormalized(data, data_count_obj,"RUNTIME_ERROR", 'rgb(2,164,174)');
-  var wrong_answer_data = createObjectToPlotDataNormalized(data, data_count_obj,"WRONG_ANSWER", 'rgb(227,79,54)');
-  var internal_error_data = createObjectToPlotDataNormalized(data, data_count_obj,"INTERNAL_ERROR", 'rgb(137,139,37)');
-  var accepted_data = createObjectToPlotDataNormalized(data, data_count_obj,"ACCEPTED", 'rgb(35,181,100)');
+  var get_function = null;
+
+  if(normalized){
+    get_function = getDataNormalized;
+  }
+  else{
+    get_function = getData;
+  }
+
+  var compilation_error_data = createObjectToPlotData(data, data_count_obj, "COMPILATION_ERROR", 'rgb(236,199,6)', get_function);
+  var time_limit_data = createObjectToPlotData(data, data_count_obj,"TIME_LIMIT_EXCEEDED", 'rgb(50,120,202)', get_function);
+  var memory_limit_data = createObjectToPlotData(data, data_count_obj,"MEMORY_LIMIT_EXCEEDED", 'rgb(119,92,133)', get_function);
+  var runtime_error_data = createObjectToPlotData(data, data_count_obj,"RUNTIME_ERROR", 'rgb(2,164,174)', get_function);
+  var wrong_answer_data = createObjectToPlotData(data, data_count_obj,"WRONG_ANSWER", 'rgb(227,79,54)', get_function);
+  var internal_error_data = createObjectToPlotData(data, data_count_obj,"INTERNAL_ERROR", 'rgb(137,139,37)', get_function);
+  var accepted_data = createObjectToPlotData(data, data_count_obj,"ACCEPTED", 'rgb(35,181,100)', get_function);
 
   var tasks_ids = [];
   var seen = new Set();
@@ -127,52 +110,6 @@ function plotVerdictStatisticsNormalized(id_div, data, statistic_title) {
 
   Plotly.purge(id_div);
   Plotly.newPlot(id_div, data, layout);
-}
-
-function plotVerdictStatisticsRaw(containerId, data, statistic_title){
-  var compilation_error_data = transformObjectToPlotData(data, "COMPILATION_ERROR", 'rgb(236,199,6)');
-  var time_limit_data = transformObjectToPlotData(data, "TIME_LIMIT_EXCEEDED", 'rgb(50,120,202)');
-  var memory_limit_data = transformObjectToPlotData(data, "MEMORY_LIMIT_EXCEEDED", 'rgb(119,92,133)');
-  var runtime_error_data = transformObjectToPlotData(data, "RUNTIME_ERROR", 'rgb(2,164,174)');
-  var wrong_answer_data = transformObjectToPlotData(data, "WRONG_ANSWER", 'rgb(227,79,54)');
-  var internal_error_data = transformObjectToPlotData(data, "INTERNAL_ERROR", 'rgb(137,139,37)');
-  var accepted_data = transformObjectToPlotData(data, "ACCEPTED", 'rgb(35,181,100)');
-
-  var tasks_ids = [];
-  var seen = new Set();
-  for(var i = 0; i < data.length; i++){
-    if(!seen.has(data[i].task_id)){
-        seen.add(data[i].task_id);
-        tasks_ids.push(data[i].task_id);
-    }
-  }
-
-  var data = [compilation_error_data, time_limit_data, memory_limit_data,
-     runtime_error_data, wrong_answer_data, internal_error_data, accepted_data];
-
-  var layout = {
-    barmode: 'stack',
-    title: statistic_title,
-    xaxis: {
-      title: 'Tasks',
-      categoryorder : "array",
-      categoryarray : tasks_ids,
-      titlefont:{
-        size: 16,
-        color: 'rgb(107,107,107)',
-
-      }
-    },
-    yaxis: {
-      title: 'Number of Sumbissions',
-      titlefont: {
-        size: 16,
-        color: 'rgb(107,107,107)'
-      }
-    }
-
-  };
-  Plotly.newPlot(containerId, data, layout);
 }
 
 var CsvConverter = (function () {
