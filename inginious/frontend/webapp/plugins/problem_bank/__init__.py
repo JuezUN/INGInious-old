0a243cd7002926f374461dc9c97154c96ea44198
Maikol Bonilla Gil <maikbogi@gmail.com>
2017-10-19 01:30:04 -0500

    Add ManageBanksCoursesApi

--- inginious/frontend/webapp/plugins/problem_bank/__init__.py
+++ inginious/frontend/webapp/plugins/problem_bank/__init__.py
@@ -8,4 +8,5 @@ _BASE_STATIC_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 
 def init(plugin_manager, course_factory, client, config):
     plugin_manager.add_page(r'/static/problem_bank/(.*)', create_static_resource_page(_BASE_STATIC_FOLDER))
     plugin_manager.add_page(r'/api/copy_task', pages.CopyTaskApi)
+    plugin_manager.add_page(r'/api/manage_banks_courses', pages.ManageBanksCoursesApi)
 
--- inginious/frontend/webapp/plugins/problem_bank/pages.py
+++ inginious/frontend/webapp/plugins/problem_bank/pages.py
@@ -5,3 +5,29 @@ from inginious.frontend.webapp.plugins.utils import AdminApi
 class CopyTaskApi(AdminApi):
     def API_GET(self):
         return 200, web.input()
+
+class ManageBanksCoursesApi(AdminApi):
+
+    def get_course_id(self):
+        parameters = web.input()
+        course_id = self.get_mandatory_parameter(parameters, "course_id")
+        self.get_course_and_check_rights(course_id)
+
+        if self.database.problem_banks.count() == 0:
+            self.database.createCollection("problem_banks", {"collation": {"locale": "en_US"}})
+
+        return course_id
+
+    def API_POST(self):
+
+        course_id = self.get_course_id()
+        self.database.problem_banks.insert({"courseid": course_id})
+
+        return 200, {"message : Bank created successfully"}
+
+    def API_DELETE(self):
+
+        course_id = self.get_course_id()
+        self.database.problem_banks.remove({"courseid": { "$eq": course_id }}, True )
+
+        return 200, {"message : Bank created successfully"}
