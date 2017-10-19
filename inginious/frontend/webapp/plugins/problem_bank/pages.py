25c003d239d4ab0106cfffb37957f25de0186a71
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-10-18 23:37:37 -0500

    Copy task implemented

--- inginious/frontend/webapp/plugins/problem_bank/pages.py
+++ inginious/frontend/webapp/plugins/problem_bank/pages.py
@@ -2,6 +2,21 @@ import web
 
 from inginious.frontend.webapp.plugins.utils import AdminApi
 
+
 class CopyTaskApi(AdminApi):
     def API_GET(self):
-        return 200, web.input()
+        parameters = web.input()
+        target_id = self.get_mandatory_parameter(parameters, "target_id")
+        bank_id = self.get_mandatory_parameter(parameters, "bank_id")
+        task_id = self.get_mandatory_parameter(parameters, "task_id")
+
+        copy_id = parameters["copy_id"] if "copy_id" in parameters else task_id
+
+        target_course = self.get_course_and_check_rights(target_id)
+        bank_course = self.get_course_and_check_rights(bank_id)
+
+        task = bank_course.get_task(task_id)
+        target_fs = self.course_factory.get_course_fs(target_id)
+        target_fs.copy_to(task.get_fs().prefix, copy_id)
+
+        return 200, {"message": "Copied succesfully"}
