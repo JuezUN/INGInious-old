d41968b7cf47b8eef7487bea4a49c29ac963160f
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-10-19 12:01:27 -0500

    Consider more cases in POST and DELETE methods

--- inginious/frontend/webapp/plugins/problem_bank/__init__.py
+++ inginious/frontend/webapp/plugins/problem_bank/__init__.py
@@ -7,8 +7,7 @@ _BASE_STATIC_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 
 
 def init(plugin_manager, course_factory, client, config):
     plugin_manager.add_page(r'/static/problem_bank/(.*)', create_static_resource_page(_BASE_STATIC_FOLDER))
-    plugin_manager.add_page(r'/api/copy_task', pages.CopyTaskApi)
-    plugin_manager.add_page(r'/api/manage_banks_courses', pages.ManageBanksCoursesApi)
+    plugin_manager.add_page(r'/api/bank_courses', pages.ManageBanksCoursesApi)
 
     if "problem_banks" not in plugin_manager.get_database().collection_names():
         plugin_manager.get_database().create_collection("problem_banks")
--- inginious/frontend/webapp/plugins/problem_bank/pages.py
+++ inginious/frontend/webapp/plugins/problem_bank/pages.py
@@ -11,14 +11,20 @@ class ManageBanksCoursesApi(AdminApi):
 
         return course_id
 
+    def already_bank(self, course_id):
+        return self.database.problem_banks.find({"courseid": {"$eq": course_id}}).count() != 0
+
     def API_POST(self):
         course_id = self.get_course_id()
-        self.database.problem_banks.insert({"courseid": course_id})
+        if not self.already_bank(course_id):
+            self.database.problem_banks.insert_one({"courseid": course_id})
 
-        return 200, {"message : Bank created successfully"}
+        return 200, {"message": "Bank created successfully"}
 
     def API_DELETE(self):
         course_id = self.get_course_id()
-        self.database.problem_banks.remove({"courseid": { "$eq": course_id }}, True )
-
-        return 200, {"message : Bank created successfully"}
+        if self.already_bank(course_id):
+            self.database.problem_banks.remove({"courseid": {"$eq": course_id}}, True)
+            return 200, {"message": "Bank removed successfully"}
+        else:
+            return 404, {"message": "No bank found"}
