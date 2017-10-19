d9729fa71e3f71dc29b3000027e9ec70ad78b9fe
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-10-19 00:06:14 -0500

    Handling more invalid cases

--- inginious/frontend/webapp/plugins/problem_bank/pages.py
+++ inginious/frontend/webapp/plugins/problem_bank/pages.py
@@ -1,7 +1,10 @@
 import web
 
+import inginious.frontend.webapp.pages.api._api_page as api
 from inginious.frontend.webapp.plugins.utils import AdminApi
-
+from inginious.common.exceptions import TaskNotFoundException
+from inginious.common.course_factory import CourseNotFoundException, CourseUnreadableException, InvalidNameException
+from inginious.common.filesystems.provider import NotFoundException
 
 class CopyTaskApi(AdminApi):
     def API_GET(self):
@@ -10,13 +13,25 @@ class CopyTaskApi(AdminApi):
         bank_id = self.get_mandatory_parameter(parameters, "bank_id")
         task_id = self.get_mandatory_parameter(parameters, "task_id")
 
-        copy_id = parameters["copy_id"] if "copy_id" in parameters else task_id
+        copy_id = parameters["copy_id"] if ("copy_id" in parameters and parameters["copy_id"] != "") else task_id
 
         target_course = self.get_course_and_check_rights(target_id)
-        bank_course = self.get_course_and_check_rights(bank_id)
 
-        task = bank_course.get_task(task_id)
+        try:
+            bank_course = self.course_factory.get_course(bank_id)
+        except (CourseNotFoundException, InvalidNameException, CourseUnreadableException):
+            raise api.APIError(400, {"error": "Invalid bank"})
+
+        try:
+            task = bank_course.get_task(task_id)
+        except TaskNotFoundException:
+            raise api.APIError(400, {"error": "Invalid task"})
+
         target_fs = self.course_factory.get_course_fs(target_id)
-        target_fs.copy_to(task.get_fs().prefix, copy_id)
+
+        try:
+            target_fs.copy_to(task.get_fs().prefix, copy_id)
+        except NotFoundException:
+            raise api.APIError(400, {"error": "the copy_id made an invalid path"})
 
         return 200, {"message": "Copied succesfully"}
