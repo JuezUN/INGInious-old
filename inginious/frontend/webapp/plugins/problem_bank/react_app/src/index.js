a79581e1ce547bd695ecbbc660934b84f2acf00c
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-11-10 02:36:11 -0500

    Add task to a coursE

--- inginious/frontend/webapp/plugins/problem_bank/react_app/src/index.js
+++ inginious/frontend/webapp/plugins/problem_bank/react_app/src/index.js
@@ -57,6 +57,85 @@ class TaskList extends React.Component{
     }
 }
 
+
+class GeneralCourseAutosuggest extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            suggestions: [],
+            value: '',
+            courses: []
+        };
+    }
+
+    getSuggestions = (value) => {
+        const normalizedValue = value.toUpperCase();
+        return this.state.courses.filter((course) => {
+            return course.name.toUpperCase().startsWith(normalizedValue) ||
+                course.id.toUpperCase().startsWith(normalizedValue);
+        });
+    }
+
+    renderSuggestion = (suggestion, {query, isHighlighted}) => {
+        return (
+            <span>{suggestion.name}</span>
+        );
+    }
+
+    onChange = (event, { newValue }) => {
+        this.setState({
+            value: newValue
+        });
+    };
+
+    updateAvailableCoursesAsync() {
+        $.getJSON("/plugins/problems_bank/api/available_courses").then((courses) => {
+            this.setState({
+                courses
+            });
+        });
+    }
+
+    componentDidMount() {
+        this.updateAvailableCoursesAsync()
+    }
+
+    addToCourse = () => {
+        let target_id = this.state.value;
+        let task_id = this.props.task_info.id;
+        let bank_id = this.props.task_info.course_id;
+
+        $.post( "/plugins/problems_bank/api/copy_task", {"target_id": target_id, "task_id": task_id, "bank_id": bank_id} ,function( data ) {
+            alert(data["message"]);
+        });
+    }
+
+    render() {
+        const inputProps = {
+            placeholder: 'Type a course name or course id',
+            value: this.state.value,
+            onChange: this.onChange
+        };
+
+        return (
+            <div>
+                <Autosuggest
+                    suggestions={this.state.suggestions}
+                    onSuggestionsFetchRequested={({value}) => this.setState({suggestions: this.getSuggestions(value)})}
+                    onSuggestionsClearRequested={() => this.setState({suggestions: []}) }
+                    getSuggestionValue={(suggestion) => suggestion.id}
+                    renderSuggestion={this.renderSuggestion}
+                    inputProps={inputProps}
+                />
+                <button onClick={this.addToCourse} class="btn btn-primary">
+                    OK
+                </button>
+            </div>
+        );
+    }
+}
+ 
 class Task extends React.Component {
     constructor(props, context) {
         super(props, context);
@@ -87,16 +166,16 @@ class Task extends React.Component {
                     bsSize="large">
 
                     <Modal.Header closeButton>
-                        <Modal.Title>Modal title</Modal.Title>
+                        <Modal.Title>Seleccione curso de destino</Modal.Title>
                     </Modal.Header>
 
                     <Modal.Body>
-                        Hey jude
+                        <GeneralCourseAutosuggest task_info={this.props.task_info} callbackParent={this.addToCourse}/>
                     </Modal.Body>
 
                     <Modal.Footer>
                         <Button onClick={this.close}>Close</Button>
-                        <Button bsStyle="primary">Save changes</Button>
+                        {/* <Button bsStyle="primary">Save changes</Button> */}
                     </Modal.Footer>
                 </Modal>
             </div>
