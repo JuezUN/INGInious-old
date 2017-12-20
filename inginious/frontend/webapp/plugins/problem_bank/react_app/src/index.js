78d00f85c2e1530a1db2fcc54dc630777a0b3cb2
Maikol Bonilla Gil <maikbogi@gmail.com>
2017-12-20 14:18:23 -0500

    Add warning message to remove bank

--- inginious/frontend/webapp/plugins/problem_bank/react_app/src/index.js
+++ inginious/frontend/webapp/plugins/problem_bank/react_app/src/index.js
@@ -417,6 +417,22 @@ class CustomAlert extends React.Component {
 
 class BankCourse extends React.Component {
 
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            showModal: false
+        };
+    }
+
+    open = () => {
+        this.setState({ showModal: true });
+    };
+
+    close = () => {
+        this.setState({ showModal: false });
+    };
+
     deleteCourse = () => {
         let course_id = this.props.name;
         let updateParent = this.props.callbackParent;
@@ -427,7 +443,9 @@ class BankCourse extends React.Component {
             success: function(data){
                 updateParent()
             }
-        })
+        });
+
+        this.close();
     };
 
     render() {
@@ -435,10 +453,32 @@ class BankCourse extends React.Component {
             <div>
                 <button type="button" className="list-group-item">
                     <b>{this.props.name}</b>
-                <a className="pull-right" onClick={this.deleteCourse} >
+                <a className="pull-right" onClick={this.open} >
                     <span className="glyphicon glyphicon-remove"></span>
                 </a>
-              </button>
+                </button>
+                <Modal className="modal-container"
+                    show={this.state.showModal}
+                    onHide={this.close}
+                    animation={true}
+                    bsSize="large">
+
+                    <Modal.Header closeButton>
+                        <Modal.Title> {this.props.name} </Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                      <Alert bsStyle="warning">
+                          <h5><strong>Are you sure you want to remove the bank privileges from this course?</strong></h5>
+                          <h6>* The course will not be remove, only the permits to be a bank will be removed.</h6>
+                      </Alert>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button onClick={this.close}>Cancel</Button>
+                        <Button onClick={this.deleteCourse} bsStyle="primary">Remove</Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         );
     }
