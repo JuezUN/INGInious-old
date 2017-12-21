5fd5ebc86bed64ec1955caba12bd642ed0779407
nicolas larranaga <nlarranagac@unal.edu.co>
2017-12-20 22:30:52 -0500

    refactored code to merge more easily with problem_bank

--- inginious/frontend/webapp/plugins/task_cache/__init__.py
+++ inginious/frontend/webapp/plugins/task_cache/__init__.py
@@ -13,9 +13,9 @@ def tag_selection_tab(course, taskid, task_data, template_helper):
     link = '<i class="fa fa-tags fa-fw"></i>&nbsp; Tags'
     template_helper.add_css("/static/task_cache/css/bootstrap-tagsinput.css")
     template_helper.add_javascript("/static/task_cache/js/bootstrap-tagsinput.js")
-    task_tags = task_data.get('task_tags', "")
+    tags = task_data.get('tags', "")
     content = template_helper.get_custom_renderer(_BASE_RENDERER_PATH,
-                                                  layout=False).task_tags(course, taskid, task_data, task_tags)
+                                                  layout=False).tags(course, taskid, task_data, tags)
     return tab_id, link, content
 
 
@@ -26,14 +26,14 @@ def init(plugin_manager, course_factory, client, config):
         descriptor = course_factory.get_course(courseid)._task_factory.get_task_descriptor_content(courseid, taskid)
         task_author = descriptor["author"]
         task_context = descriptor["context"]
-        task_tags = new_content.get("task_tags", "").split(',')
+        tags = new_content.get("tags", "").split(',')
         task_data = {
             "task_name": task_name,
             "task_id": taskid,
             "task_author": task_author,
             "task_context": task_context,
             "course_id": courseid,
-            "task_tags": task_tags
+            "tags": tags
         }
 
         data_filter = {
