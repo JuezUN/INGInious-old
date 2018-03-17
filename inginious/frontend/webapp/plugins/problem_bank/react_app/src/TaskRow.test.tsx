2f5f3cd2a3c7cb2819574f16e2aab5a49b697431
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2018-03-16 19:55:15 -0500

    Jest Fixed

--- inginious/frontend/webapp/plugins/problem_bank/react_app/src/TaskRow.test.tsx
+++ inginious/frontend/webapp/plugins/problem_bank/react_app/src/TaskRow.test.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
+import { shallow, ShallowWrapper } from 'enzyme';
 
 describe('TaskRow', () => {
-    
+    let wrapper: ShallowWrapper<{}, {}>
+
+    beforeEach(() => {
+        wrapper = shallow(<div/>);
+    })
 
     it('Renders properly', () => {
-
+        expect(wrapper).toMatchSnapshot();
     });
 });
 
